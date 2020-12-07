79642aa3738193dea883cf466b3ad310845e912c
companion <pandonautical@gmail.com>
2020-12-07 03:24:57 +0000

    working on auth headers

--- little-db-companion/db-commands.js
+++ little-db-companion/db-commands.js
@@ -19,32 +19,23 @@ async function login (name, pass) {
             username: name,
             password: pass
         });
-        console.log(response.data);
+        //console.log(response.data);
         return response.data;
     } catch (err) {
         console.log(err);
     }
 }
 
-async function set (table, key, value) {
+async function set (table, key, value, token) {
     try {
         response = await axios.post('https://ldbc.cf/set', {
-            table: table,
-            key: key,
-            value: value
-        });
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function get (table, key) {
-    try {
-        response = await axios.get('https://ldbc.cf/get', {
-            params: {
+            body: {
                 table: table,
-                key: key
+                key: key,
+                value: value
+            },
+            headers: {
+                'authorization' : token
             }
         });
         return response.data;
@@ -53,6 +44,41 @@ async function get (table, key) {
     }
 }
 
+async function get (table, key, token) {
+    try {
+        response = await axios.get('https://ldbc.cf/get', {
+            params: {
+                table: table,
+                key: key
+            },
+            headers: {
+                'authorization' : token
+            }
+        });
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+/*
+class login {
+  constructor (name, password) {
+    try {
+        response = await axios.post('https://ldbc.cf/login', {
+            username: name,
+            password: pass
+        });
+        console.log(response.data);
+        this.token = response.data;
+    } catch (err) {
+        console.log(err);
+    }
+  }
+  get token() {
+    return this.token;
+  }
+}
+*/
 module.exports = {
     register,
     login,
